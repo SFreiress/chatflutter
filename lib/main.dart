94752a13bc73076d17ced203df47b8db79d382f8
Sérgio Freire Soares <sergiofss_2005@hotmail.com>
2024-04-26 21:59:10 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_chat/screens/contacts_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,12 +7,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Chat',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.amber,
         useMaterial3: true,
       ),
       darkTheme: ThemeData.dark(
@@ -19,51 +21,6 @@ class MyApp extends StatelessWidget {
       ),
       themeMode: ThemeMode.system,
       home: const ContactsPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
--- lib/screens/chat_page.dart
+++ lib/screens/chat_page.dart
@@ -3,20 +3,26 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
-import 'package:flutter_chat_ui/flutter_chat_types.dart' as types;
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_client/web_socket_client.dart';
+import 'package:web_socket_channel/io.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key, required this.name});
+  const ChatPage({Key? key, required this.name, required this.id})
+      : super(key: key);
 
   final String name;
+  final String id;
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  final socket = WebSocket(Uri.parse('ws://echo.websocket.org'));
+  final socket =
+      WebSocket(Uri.parse('ws://10.200.74.90:8765')); // If using emulator
+
   final List<types.Message> _messages = [];
   late types.User otherUser;
   late types.User me;
@@ -24,20 +30,97 @@ class _ChatPageState extends State<ChatPage> {
   @override
   void initState() {
     super.initState();
-    otherUser = types.User(id: widget.name, firstName: widget.name);
-    me = const types.User(id: 'sergio', firstName: 'sergio');
-
+    me = types.User(id: widget.id, firstName: widget.id);
+    // Listen to messages from the server.
     socket.messages.listen((incomingMessage) {
-      List<String> parts = incomingMessage.split('from ');
+      // Split the response into the JSON string and the "from" string
+      List<String> parts = incomingMessage.split(' from ');
       String jsonString = parts[0];
 
+      // Parse the JSON string using the jsonDecode() function
       Map<String, dynamic> data = jsonDecode(jsonString);
 
+      // Access the values from the parsed JSON object
       String id = data['id'];
       String msg = data['msg'];
       String timestamp = data['timestamp'];
 
-      onMessageReceived(msg);
+      if (id != me.id) {
+        otherUser = types.User(id: id, firstName: id);
+        onMessageReceived(msg);
+      }
     });
+  }
+
+  String randomString() {
+    final random = Random.secure();
+    final values = List<int>.generate(16, (i) => random.nextInt(255));
+    return base64UrlEncode(values);
+  }
+
+  void onMessageReceived(String message) {
+    var newMessage = types.TextMessage(
+      author: otherUser,
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      text: message,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      metadata: {
+        'senderName': otherUser.firstName,
+      },
+    );
+    _addMessage(newMessage);
+  }
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: me,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+      metadata: {
+        'senderName': me.firstName,
+      },
+    );
+
+    var payload = {
+      'id': me.id,
+      'msg': message.text,
+      'nick': me.firstName,
+      'timestamp': DateTime.now().millisecondsSinceEpoch.toString(),
+    };
+
+    socket.send(json.encode(payload));
+
+    _addMessage(textMessage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ID ${widget.id} NickName ${widget.name}'),
+      ),
+      body: Chat(
+        messages: _messages,
+        onSendPressed: _handleSendPressed,
+        user: me,
+        theme: DefaultChatTheme(
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    // Close the connection.
+    socket.close();
+    super.dispose();
   }
 }
